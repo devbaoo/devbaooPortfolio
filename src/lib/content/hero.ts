51e247dc1f2f42686c83822796291fb4f89c00cb
devbaoo <caokhacbao0712@gmail.com>
2026-01-17 22:02:02 +0700

    feat: update

--- src/lib/content/hero.ts
+++ src/lib/content/hero.ts
@@ -1,15 +1,15 @@
-import { HeroSectionType } from '@/lib/types/sections';
-import { resumeFileName } from '@/lib/utils/config';
+import { HeroSectionType } from "@/lib/types/sections";
+import { resumeFileName } from "@/lib/utils/config";
 
 export const heroSection: HeroSectionType = {
-  subtitle: 'Hi, my name is',
-  title: 'devbaoo.',
-  tagline: '',
+  subtitle: "Hi, my name is",
+  title: "devbaoo.",
+  tagline: "",
   description:
     "I'm a passionate Full-Stack developer with experience in React.js, JavaScript, TypeScript, and TailwindCSS. On the back-end, I work with C#, Python, and Node.js to build RESTful APIs and deploy them on various platforms.",
-  specialText: 'Currently available for freelance',
+  specialText: "Currently available for freelance",
   cta: {
-    title: 'see my resume',
+    title: "see my resume",
     url: `/${resumeFileName}`,
     hideInDesktop: true,
   },
